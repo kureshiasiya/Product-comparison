9edd5be485ec2ad033f394ecc045c84387abd006
kureshiasiya <kureshiasiya2@gmail.com>
2024-08-10 13:39:12 +0530

    restore

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductChooser from "./ProductChooser";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductChooser />
+    </div>
+  );
+}
+
+export default App;
